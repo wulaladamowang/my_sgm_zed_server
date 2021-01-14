bb76f0889a5c1c2b08850da5d78299fbe7070cb4
wulaladamowang <317670404@qq.com>
2021-01-14 23:00:05 +0800

    获得mask 与视差图的结果图

--- my_sgm_zed/src/main.cpp
+++ my_sgm_zed/src/main.cpp
@@ -3,12 +3,13 @@
 #include "get_roi.hpp"
 #include "get_disparity.hpp"
 #include "little_tips.hpp"
+#include <thread>
 #include <string>
 #include <iostream>
 
 int main(int argc, char** argv){
 
-
+// zed相机的初始化
 	sl::Camera zed;
 	sl::InitParameters initParameters;
 	initParameters.camera_resolution = sl::RESOLUTION::HD1080;
@@ -22,13 +23,14 @@ int main(int argc, char** argv){
     sl::RuntimeParameters runtime_parameters;
     runtime_parameters.sensing_mode = sl::SENSING_MODE::LAST;
 
+//图像变量的获取及初始化
     const int width = static_cast<int>(zed.getCameraInformation().camera_resolution.width);
 	const int height = static_cast<int>(zed.getCameraInformation().camera_resolution.height);
     sl::Mat zed_image_l(zed.getCameraInformation().camera_resolution, sl::MAT_TYPE::U8_C1);
-	sl::Mat zed_image_r(zed.getCameraInformation().camera_resolution, sl::MAT_TYPE::U8_C1);
+	sl::Mat zed_image_r(zed.getCameraInformation().camera_resolution, sl::MAT_TYPE::U8_C1);//相机原格式获得图像
     cv::Mat img_zed_left = slMat2cvMat(zed_image_l);
-    cv::Mat img_zed_right = slMat2cvMat(zed_image_r);
-    cv::Mat img_zed_left_remap;
+    cv::Mat img_zed_right = slMat2cvMat(zed_image_r);//sl::Mat 到opencv 格式图像的转换
+    cv::Mat img_zed_left_remap;// 存储校正畸变后的图像
     cv::Mat img_zed_right_remap;
 
 
@@ -36,6 +38,7 @@ int main(int argc, char** argv){
     const int disp_size = (argc >= 3) ? std::stoi(argv[2]) : 128;//默认的disparity size, 可选择64,128,256
     const bool subpixel = (argc >= 4) ? std::stoi(argv[3]) != 0 : true;//是否使用subpixel
 
+// 相机内外参数的读取, 注意相机内外参数要与使用的相机型号或拍摄视频的相机型号相一致
     std::string in = "/home/wang/code/c++Code/my_sgm_zed_server/canshu/intrinsics.yml";
     cv::FileStorage fs(in, cv::FileStorage::READ);
     if(!fs.isOpened())
@@ -68,10 +71,10 @@ int main(int argc, char** argv){
 
     // cv::Mat img_left = cv::imread(argv[1], -1);
     // cv::Mat img_right = cv::imread(argv[2], -1);
-    cv::Size siz = cv::Size(width*scale, height*scale);
-    cv::Mat img_zed_left_scale, img_zed_right_scale;
-    cv::Mat disparity(siz, CV_16S);
-    cv::Mat disparity_8u, disparity_32f, disparity_color, draw;
+    cv::Size siz = cv::Size(width*scale, height*scale);// 对于原图进行缩放尺寸
+    cv::Mat img_zed_left_scale, img_zed_right_scale;// 缩放之后的图像, 在校正之后的图像基础上进行缩放
+    cv::Mat disparity(siz, CV_16S);//存储sgm算法获得的视差图
+    cv::Mat disparity_8u, disparity_32f, disparity_mask;// 8u转化便于显示的灰度图, 32f实际视差图,带小数点, disparity_mask保留mask区域的视差
     char key = ' ';
     long long ts0 =getCurrentTime();
     long long ts1 =getCurrentTime();
@@ -81,14 +84,12 @@ int main(int argc, char** argv){
 
     long long ts5 =getCurrentTime();
     long long ts6 = getCurrentTime();
-    bool has_roi = false;
-    cv::Mat mask;
-    std::vector<int> rect_roi;
+    bool has_roi = false;// 是否检测到roi
+    cv::Mat mask(img_size, CV_8U);// 检测到的roi, 用mask 标记出来
+    cv::Mat mask_scale(siz, CV_8U);
+    std::vector<int> rect_roi;// 存储包围目标区域的最小正矩形的四个像素坐标
+
     while('q' != key){
-
-        
-        std::cout << zed.getCameraInformation().serial_number << std::endl;
-
         if (zed.grab(runtime_parameters) == sl::ERROR_CODE::SUCCESS) {
             ts0 =getCurrentTime();
             // Retrieve the left image, depth image in half-resolution
@@ -96,15 +97,16 @@ int main(int argc, char** argv){
             zed.retrieveImage(zed_image_r, sl::VIEW::RIGHT_UNRECTIFIED_GRAY, sl::MEM::CPU);
             ts1 = getCurrentTime();
             std::cout << "get image: " << (ts1-ts0) << "毫秒" << std::endl;
+
             cv::remap(img_zed_left, img_zed_left_remap, map11, map12, cv::INTER_LINEAR);
             cv::remap(img_zed_right, img_zed_right_remap, map21, map22, cv::INTER_LINEAR);
-
             cv::resize(img_zed_left_remap, img_zed_left_scale, siz, cv::INTER_LINEAR);
             cv::resize(img_zed_right_remap, img_zed_right_scale, siz, cv::INTER_LINEAR);
             ts2 = getCurrentTime();
             std::cout << "Process image: " << (ts2-ts1) << "毫秒" << std::endl;
             
             get_roi(std::ref(img_zed_left_remap), std::ref(mask), std::ref(has_roi), std::ref(rect_roi));
+            cv::resize(mask, mask_scale, siz, cv::INTER_LINEAR);
             ts3 = getCurrentTime();
             std::cout << "roi image: " << (ts3-ts2) << "毫秒" << std::endl;
 
@@ -112,36 +114,19 @@ int main(int argc, char** argv){
             disparity.convertTo(disparity_32f, CV_32F, subpixel ? 1. / sgm::StereoSGM::SUBPIXEL_SCALE : 1);
             ts4 = getCurrentTime();
             std::cout << "disparity image: " << (ts4-ts3) << "毫秒" << std::endl;
+
             ts5 = getCurrentTime();
             std::cout << "total: " << (ts5-ts6) << "毫秒" << std::endl;
             std::cout << "-------------" << std::endl;
-
             disparity_32f.convertTo(disparity_8u, CV_8U, 255. / 64);
-            cv::applyColorMap(disparity_8u, disparity_color, cv::COLORMAP_JET);
-            disparity_color.setTo(cv::Scalar(0, 0, 0), disparity_32f < 0); 
-            // std::cout << disparity_32f.ptr<float>(siz.height/2)[siz.width/2] << std::endl;
-            // cv::circle(disparity_color, cv::Point( siz.width/2,siz. height/2), 10, cv::Scalar(255, 255, 255));
+            disparity_mask = disparity_8u.mul(mask_scale);
             cv::imshow("left image", img_zed_left_scale);
             cv::imshow("disparity 1", disparity_8u);
-            cv::imshow("disparity", disparity_color);  
+            cv::imshow("mask", disparity_mask);
             key = cv::waitKey(1);
             ts6 = getCurrentTime();
         }
     }
-    
-
-    
-
-    // bool has_roi = false;
-    // cv::Mat mask = cv::Mat(img_left.size(), CV_8U);
-    // std::vector<int> rect_roi;
-    // get_roi(std::ref(img_left), std::ref(mask), std::ref(has_roi), std::ref(rect_roi));
-    // cv::Mat dst = disparity.mul(mask);
-    // cv::imshow("mask", mask*255);
-    // cv::imshow("disparity", disparity_8u);
-    // cv::imshow("disparity_color", disparity_color);
-    // cv::imshow("dst", dst);
-    // cv::imshow("img_left", img_left);
     zed.close();
     cv::destroyAllWindows();
 
